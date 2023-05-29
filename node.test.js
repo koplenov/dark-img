f9cf90da5652a4da894d54cef17a3562cccee7cc
koplenov <koplenov@gmail.com>
2023-05-29 20:03:03 +0000

    Deploying to gh-pages from @ koplenov/dark-img@2895171cfff8314ea31dedb48446c4d7b6d5517d 🚀

--- node.test.js
+++ node.test.js
@@ -3830,7 +3830,7 @@ var $;
                 return $mol_fetch.json(this.src());
             }
             img_src() {
-                return this.picture_info().src;
+                return this.picture_info().thumbnail;
             }
             img_width() {
                 return Number(this.picture_info().size.split('x')[0]);
--- web.mjs
+++ web.mjs
@@ -3285,7 +3285,7 @@ var $;
                 return $mol_fetch.json(this.src());
             }
             img_src() {
-                return this.picture_info().src;
+                return this.picture_info().thumbnail;
             }
             img_width() {
                 return Number(this.picture_info().size.split('x')[0]);
