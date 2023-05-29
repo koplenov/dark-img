ce605f1d2c1109c3e6cbe012015772158dbfee95
koplenov <koplenov@gmail.com>
2023-05-29 22:42:37 +0300

    fix build

--- view.tree.ts
+++ view.tree.ts
@@ -4,14 +4,14 @@ namespace $.$$ {
 		picture_info(){
 			return $mol_fetch.json(this.src())
 		}
-		img_src() : string {
-			return this.picture_info().src as string
+		img_src() {
+			return this.picture_info().src
 		}
 		img_width(){
-			return this.picture_info().size.split('x')[0]
+			return Number(this.picture_info().size.split('x')[0])
 		}
 		img_height(){
-			return this.picture_info().size.split('x')[1]
+			return Number(this.picture_info().size.split('x')[1])
 		}
 	}
 }
