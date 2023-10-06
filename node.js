f01617ec9d6db332b6939bdac24d292453650954
koplenov <koplenov@users.noreply.github.com>
2023-10-06 07:11:01 +0000

    Deploying to gh-pages from @ koplenov/dark-img@7eb422c160ca800a174f6b47e12f6aea9cd93b4d 🚀

--- node.js
+++ node.js
@@ -5547,6 +5547,9 @@ var $;
                 ]);
                 el.selectionEnd = to;
                 el.selectionStart = from;
+                if (to !== from && el.selectionEnd === el.selectionStart) {
+                    el.selectionEnd = to;
+                }
             }
             selection_start() {
                 const el = this.dom_node();
