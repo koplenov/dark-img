258378eb5bb7ff844cac73bd180803e2988399fe
koplenov <deploy-bot>
2025-01-03 07:13:52 +0000

    Deploying to gh-pages from @ koplenov/dark-img@d69934a21992819e020d7d2ca827b7b1cef1aa7d 🚀

--- node.test.js
+++ node.test.js
@@ -7480,7 +7480,7 @@ var $;
 			return "async";
 		}
 		cors(){
-			return "anonymous";
+			return null;
 		}
 		natural_width(){
 			return 0;
--- web.mjs
+++ web.mjs
@@ -6973,7 +6973,7 @@ var $;
 			return "async";
 		}
 		cors(){
-			return "anonymous";
+			return null;
 		}
 		natural_width(){
 			return 0;
